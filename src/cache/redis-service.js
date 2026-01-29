eeb769abdabef6c0f4343485619bb688ca6b6e9b
Rizal <rizalbario3@gmail.com>
2026-01-30 05:15:08 +0700

    fix: redis-service get code

---
+++ src/cache/redis-service.js
@@ -0,0 +1,17 @@
+class CacheService {
+	constructor() {
+	this._client = createClient({
+	socket: {
+		host: process.env.REDIS_SERVER,
+		
+	},
+	});
+
+	this._client.on('error', (error) => {
+		console.error(error);
+	});
+
+	this._client.connect();
+	
+  }
+}
